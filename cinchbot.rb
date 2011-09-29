5fd2e79392c4cebc309ddf4a28125ccc3474af55
Nick Markwell <nick@duckinator.net>
2011-09-28 20:37:36 -0400

    Everything but quits works

--- cinchbot.rb
+++ cinchbot.rb
@@ -49,9 +49,40 @@ class Log
 
   def self.add(m)
     @@m = m
-    File.open(file, 'w') do |f|
+    File.open(file, 'a') do |f|
       time = Time.now.strftime('%T')
-      f.write("[#{time}] <#{@@m.user.nick}> #{@@m.message}")
+      chan = false
+      str  = nil
+      msg  = m.message
+      
+      case m.command
+      when 'NOTICE'
+        fmt = "[%s] -%s- %s"
+      when 'JOIN'
+        chan = true
+        fmt = "[%s] * %s has joined %s"
+      when 'PART'
+        chan = true
+        fmt = "[%s] * %s has left %s (%s)"
+      when 'QUIT'
+        fmt = "[%s] * %s has quit (%s)"
+      else
+        if msg[0..6] == "\x01ACTION"
+          msg = msg[7..-1]
+          msg.delete!("\x01")
+          fmt = "[%s] * %s %s"
+        else
+          fmt = "[%s] <%s> %s"
+        end
+      end
+      
+      p fmt
+      if chan
+        str = fmt % [time, @@m.user.nick, @@m.channel.name, msg]
+      else
+        str = fmt % [time, @@m.user.nick, msg]
+      end
+      f.write(str + "\r\n")
     end
   end
 
@@ -71,7 +102,19 @@ bot = Cinch::Bot.new do
     c.plugins.plugins = [Cinch::Plugins::BasicCTCP]
     c.plugins.options[Cinch::Plugins::BasicCTCP][:commands] = [:version, :time, :ping]
   end
-    
+
+  on :join do |m|
+    Log << m
+  end
+
+  on :part do |m|
+    Log << m
+  end
+
+  on :quit do |m|
+    Log << m
+  end
+
   on :message do |m|
     Log << m
   end
