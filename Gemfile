878a2ddae16380c7f703e41742886dd309068b2e
N3bTcx <benoit@hopsandfork.com>
2014-11-02 20:16:57 +0100

    Add json output.
    Quite ruff, need more work. Will try to plug bootstrap datatables to filter, sort logs and also D3.js.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,8 @@ gem 'sqlite3', '1.3.9'
 gem 'cinch', '2.1.0'
 gem 'sinatra', '1.4.5'
 gem 'configru', '3.6.0'
+
+group :test do
+	gem "rack-test"
+	gem "pry"
+end
--- webserver.rb
+++ webserver.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
-require './logger'
 require 'sqlite3'
+require 'json'
+require './logger'
+
 
 db = SQLite3::Database.open "irclogs.db"
 
 get '/' do
   @dbcont = db.execute("SELECT * From irclogs;").reverse
   erb :index
+end
+
+get '/logs.json' do
+  content_type :json
+  @dbcont = db.execute("SELECT * From irclogs;")
+  @dbcont.to_json
 end
